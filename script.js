81e1cb96efc84b638b55b78dc2645f9a1ea15ed1
AHShawonOfficial <stushawon@gmail.com>
2022-03-30 16:21:42 +0600

    fixed styling

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const resetBtn = document.querySelector('.reset');
 const errorBox = document.querySelector('.error');
 
 startBtn.addEventListener('click', () => {
-   const err = timer.start(input.value);
+   const err = timer.start(input.value, () => {
+      errorBox.textContent = 'Countdown Completed(From callback function)';
+      errorBox.style.display = 'block';
+   });
    if (!err) {
       errorBox.style.display = 'none';
    } else {
-      errorBox.style.display = 'block';
       errorBox.textContent = err;
    }
 });
